4427644ff65496647ef29d6528ba913f5bfe2321
Daniel Ngahan <ngahan_d@epita.fr>
2017-10-07 17:40:10 +0200

    [model][views] - create model and view

--- fortunes/models.py
+++ fortunes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    pseudo = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
+class Fortune(models.Model):
+    id = models.AutoField(primary_key=True)
+    text = models.CharField(max_length=1000)
+    score = models.IntegerField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    date =  models.DateField(default=datetime.now)
