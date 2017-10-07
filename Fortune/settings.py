23bbee317f448d409893a3242532752c29fee73e
Daniel Ngahan <ngahan_d@epita.fr>
2017-10-07 18:26:02 +0200

    [views] create view and controller

--- Fortune/settings.py
+++ Fortune/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME':'postgres',
-        'PASSWORD':'danidan',
+        'PASSWORD':'XXX',
         'USER':'postgres',
         'HOST':'127.0.0.1',
         'PORT':'5432',
